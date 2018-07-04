2c57ae10b9bbc8d2e12842f790547f6dc3e2da28
yuto <kfc.kfc.kfc.kfc.kfc.2014@gmail.com>
2018-07-04 16:12:52 +0900

    remove migrate

--- db/migrate/20180703075601_add_lastaccess_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddLastaccessToUsers < ActiveRecord::Migration[5.0]
-  def change
-    add_column :users, :lastaccess, :time, default: 0
-  end
-end
--- db/migrate/20180704050147_remove_lastaccess_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveLastaccessFromUsers < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :users, :lastaccess, :time
-  end
-end

8ae51c18136e916c9da9e550c8ceb3e272250bd1
yuto <kfc.kfc.kfc.kfc.kfc.2014@gmail.com>
2018-07-04 16:38:34 +0900

    change type

--- db/migrate/20180704023505_add_accesstime_to_users.rb
+++ db/migrate/20180704023505_add_accesstime_to_users.rb
@@ -1,5 +1,5 @@
 class AddAccesstimeToUsers < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :accesstime, :DateTime, default:Time.local(1999,1,1,0,0,0)
+    add_column :users, :accesstime, :Time, default:Time.local(1999,1,1,0,0,0)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -45,8 +45,8 @@ ActiveRecord::Schema.define(version: 20180704050410) do
     t.datetime "activated_at"
     t.string   "reset_digest"
     t.datetime "reset_sent_at"
-    t.datetime "accesstime",        default: '1998-12-31 15:00:00'
-    t.datetime "lastaccesshome",    default: '1998-12-31 15:00:00'
+    t.time     "accesstime",        default: '2000-01-01 15:00:00'
+    t.time     "lastaccesshome",    default: '2000-01-01 15:00:00'
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
